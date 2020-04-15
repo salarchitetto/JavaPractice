1a48716d76d2c90084be59d0ab7a8a25023facf0
salarchitetto <sal.architetto862@gmail.com>
2020-04-14 22:24:36 -0400

    quick update

--- src/main/java/com/github/javaPractice/kafka/twitterKafka/ElasticConfigs.java
+++ src/main/java/com/github/javaPractice/kafka/twitterKafka/ElasticConfigs.java
@@ -3,7 +3,7 @@ package com.github.javaPractice.kafka.twitterKafka;
 public class ElasticConfigs {
 
 
-    String ELASTIC_HOST_NAME = "test-cluster-1545751398.us-east-1.bonsaisearch.net";
-    String ELASTIC_USER_NAME = "bbajjbvfzm";
-    String ELASTIC_PASSWORD = "6jpgss7x1s";
+    String ELASTIC_HOST_NAME = "";
+    String ELASTIC_USER_NAME = "";
+    String ELASTIC_PASSWORD = "";
 }
